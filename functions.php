19c4b9e9127bd573fe4f41ec0bf87b670dc96bc8
Yunus AK <akyunus@users.noreply.github.com>
2017-01-23 11:24:57 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,5 +6,16 @@ function hangi_gun($tarih) {
   return $gunadi[date('w', strtotime($tarih))];
 }
 
+// belirtilen yilin belirtilen ayı kaç gün çeker
+function ay_kac_gun($yil,$ay) {
+  return cal_days_in_month(CAL_GREGORIAN, $ay, $yil);
+}
+
+function buay_kac_gun(){
+  $yil = date('Y');
+  $ay = date('n');
+  return ay_kac_gun($yil,$ay);
+}  
 
 echo hangi_gun('2017/01/23');
+echo ay_kac_gun(2015,1);
