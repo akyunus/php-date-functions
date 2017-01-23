2656574b888aa512a5b47698fc25a425891abcc2
Yunus AK <akyunus@users.noreply.github.com>
2017-01-23 11:18:18 +0300

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,7 @@
+function hangi_gun($tarih) {
+  $gunadi=array('Pazar','Pazartesi','Salı','Çarşamba','Perşembe','Cuma','Cumartesi');
+  return $gunadi[date('w', strtotime($tarih))];
+}
+
+
+echo hangi_gun('2017/01/23');
